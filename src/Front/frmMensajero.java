ea660cffd181b73d9db737bbc1c5320387a77539
edwin <edwin@192.168.0.105>
2023-11-12 06:29:47 -0500

    pequeños cambios visuales

--- src/Front/frmMensajero.java
+++ src/Front/frmMensajero.java
@@ -1,22 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
 package Front;
 
-import java.awt.Image;
-import java.awt.Toolkit;
-import javax.swing.ImageIcon;
-
+/**
+ *
+ * @author edwin
+ */
 public class frmMensajero extends javax.swing.JFrame {
 
+    /**
+     * Creates new form frmAdmins
+     */
     public frmMensajero() {
         initComponents();
         this.setLocationRelativeTo(this);
-        this.setSize(1024, 600);
-        
-        Image favi = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/vino.png"));
-        this.setIconImage(favi);
-        
-        Image logo = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/vino.png"));
-        lblLogo.setIcon(new ImageIcon(logo.getScaledInstance(lblLogo.getWidth(), 
-                lblLogo.getHeight(), Image.SCALE_SMOOTH)));
     }
 
     /**
@@ -28,98 +27,157 @@ public class frmMensajero extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        pnlTienda = new javax.swing.JPanel();
-        btnRegistrarUsu = new javax.swing.JButton();
-        btnProductos = new javax.swing.JButton();
-        lblLogo = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        btnRegistrarUsu1 = new javax.swing.JButton();
+        btnRegistrarUsu2 = new javax.swing.JButton();
+        btnRegistrarUsu3 = new javax.swing.JButton();
+        btnRegistrarUsu4 = new javax.swing.JButton();
+        btnRegistrarUsu5 = new javax.swing.JButton();
+        lblLogo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Mensajero");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setResizable(false);
 
-        pnlTienda.setBackground(new java.awt.Color(255, 255, 255));
-
-        btnRegistrarUsu.setBackground(new java.awt.Color(102, 0, 102));
-        btnRegistrarUsu.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
-        btnRegistrarUsu.setForeground(new java.awt.Color(255, 255, 255));
-        btnRegistrarUsu.setText("GESTIÓN DE USUARIOS");
-        btnRegistrarUsu.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRegistrarUsuActionPerformed(evt);
-            }
-        });
-
-        btnProductos.setBackground(new java.awt.Color(102, 0, 102));
-        btnProductos.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
-        btnProductos.setForeground(new java.awt.Color(255, 255, 255));
-        btnProductos.setText("GESTIÓN DE PRODUCTO");
-        btnProductos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnProductosActionPerformed(evt);
-            }
-        });
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Helvetica Neue", 3, 36)); // NOI18N
-        jLabel1.setText("                    Mensajero De Dionisio´s");
+        jLabel1.setText("    Mensajero De Dionisio´s");
 
-        javax.swing.GroupLayout pnlTiendaLayout = new javax.swing.GroupLayout(pnlTienda);
-        pnlTienda.setLayout(pnlTiendaLayout);
-        pnlTiendaLayout.setHorizontalGroup(
-            pnlTiendaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-            .addGroup(pnlTiendaLayout.createSequentialGroup()
-                .addGap(41, 41, 41)
-                .addComponent(btnProductos, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 90, Short.MAX_VALUE)
-                .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(110, 110, 110)
-                .addComponent(btnRegistrarUsu, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(49, 49, 49))
-            .addGroup(pnlTiendaLayout.createSequentialGroup()
-                .addGap(55, 55, 55)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        btnRegistrarUsu1.setBackground(new java.awt.Color(0, 0, 255));
+        btnRegistrarUsu1.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
+        btnRegistrarUsu1.setForeground(new java.awt.Color(255, 255, 255));
+        btnRegistrarUsu1.setText("VENTAS");
+        btnRegistrarUsu1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarUsu1ActionPerformed(evt);
+            }
+        });
+
+        btnRegistrarUsu2.setBackground(new java.awt.Color(0, 0, 255));
+        btnRegistrarUsu2.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
+        btnRegistrarUsu2.setForeground(new java.awt.Color(255, 255, 255));
+        btnRegistrarUsu2.setText("GESTIÓN DE USUARIOS");
+        btnRegistrarUsu2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarUsu2ActionPerformed(evt);
+            }
+        });
+
+        btnRegistrarUsu3.setBackground(new java.awt.Color(0, 0, 255));
+        btnRegistrarUsu3.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
+        btnRegistrarUsu3.setForeground(new java.awt.Color(255, 255, 255));
+        btnRegistrarUsu3.setText("GESTIÓN DE CLIENTES");
+        btnRegistrarUsu3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarUsu3ActionPerformed(evt);
+            }
+        });
+
+        btnRegistrarUsu4.setBackground(new java.awt.Color(0, 0, 255));
+        btnRegistrarUsu4.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
+        btnRegistrarUsu4.setForeground(new java.awt.Color(255, 255, 255));
+        btnRegistrarUsu4.setText("GESTIÓN DE PROVEEDORES");
+        btnRegistrarUsu4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarUsu4ActionPerformed(evt);
+            }
+        });
+
+        btnRegistrarUsu5.setBackground(new java.awt.Color(0, 0, 255));
+        btnRegistrarUsu5.setFont(new java.awt.Font("Lucida Sans", 1, 14)); // NOI18N
+        btnRegistrarUsu5.setForeground(new java.awt.Color(255, 255, 255));
+        btnRegistrarUsu5.setText("GESTIÓN DE PRODUCTOS");
+        btnRegistrarUsu5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarUsu5ActionPerformed(evt);
+            }
+        });
+
+        lblLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Vino.png"))); // NOI18N
+        lblLogo.setMaximumSize(new java.awt.Dimension(300, 300));
+        lblLogo.setMinimumSize(new java.awt.Dimension(300, 300));
+        lblLogo.setPreferredSize(new java.awt.Dimension(300, 300));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 543, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnRegistrarUsu3, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnRegistrarUsu2, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnRegistrarUsu4, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnRegistrarUsu5, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnRegistrarUsu1, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
-        pnlTiendaLayout.setVerticalGroup(
-            pnlTiendaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pnlTiendaLayout.createSequentialGroup()
-                .addGap(27, 27, 27)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(pnlTiendaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(pnlTiendaLayout.createSequentialGroup()
-                        .addGap(57, 57, 57)
-                        .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 259, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(pnlTiendaLayout.createSequentialGroup()
-                        .addGap(76, 76, 76)
-                        .addComponent(btnProductos, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(pnlTiendaLayout.createSequentialGroup()
-                        .addGap(77, 77, 77)
-                        .addComponent(btnRegistrarUsu, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(162, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnRegistrarUsu2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnRegistrarUsu3, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnRegistrarUsu4, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnRegistrarUsu5, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnRegistrarUsu1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlTienda, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(pnlTienda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnRegistrarUsuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarUsuActionPerformed
-        //frmUsuarios venUsuarios = new frmUsuarios();
-        //venUsuarios.setVisible(true);
-    }//GEN-LAST:event_btnRegistrarUsuActionPerformed
+    private void btnRegistrarUsu1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarUsu1ActionPerformed
+          frmUsers venUser = new frmUsers();
+        venUser.setVisible(true);
+    }//GEN-LAST:event_btnRegistrarUsu1ActionPerformed
 
-    private void btnProductosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProductosActionPerformed
+    private void btnRegistrarUsu2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarUsu2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnProductosActionPerformed
+    }//GEN-LAST:event_btnRegistrarUsu2ActionPerformed
+
+    private void btnRegistrarUsu3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarUsu3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegistrarUsu3ActionPerformed
+
+    private void btnRegistrarUsu4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarUsu4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegistrarUsu4ActionPerformed
+
+    private void btnRegistrarUsu5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarUsu5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegistrarUsu5ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -160,10 +218,13 @@ public class frmMensajero extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnProductos;
-    private javax.swing.JButton btnRegistrarUsu;
+    private javax.swing.JButton btnRegistrarUsu1;
+    private javax.swing.JButton btnRegistrarUsu2;
+    private javax.swing.JButton btnRegistrarUsu3;
+    private javax.swing.JButton btnRegistrarUsu4;
+    private javax.swing.JButton btnRegistrarUsu5;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblLogo;
-    private javax.swing.JPanel pnlTienda;
     // End of variables declaration//GEN-END:variables
 }
